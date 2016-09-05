c75f99fccdba5b4c52bb83f90a0b533dec2cdf48
PointlessFTW <harveyorme@gmail.com>
2016-09-05 13:57:00 +0100

    Update 9-Move it on back.py

--- Language Skills/Python/Unit 03 Conditionals and Control Flow/02 PygLatin/PygLatin PART2/9-Move it on back.py
+++ Language Skills/Python/Unit 03 Conditionals and Control Flow/02 PygLatin/PygLatin PART2/9-Move it on back.py
@@ -1,12 +1,1 @@
-pyg = 'ay'
-
-original = raw_input('Enter a word:')
-
-if len(original) > 0 and original.isalpha():
-    word = original.lower()
-    first = word[0]
-
-    new_word = word + first + pyg
-    print pyg
-else:
-    print 'empty
+new_word = word + first + pyg
